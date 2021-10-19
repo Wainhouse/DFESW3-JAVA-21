4edd443c7687c344e33d945de23523b9c890cf9b
Luke Wainhouse <lukewainhouse@hotmail.com>
2021-10-19 15:22:49 +0200

    Changes to operators

--- Operators/src/com/qa/main/Operators.java
+++ Operators/src/com/qa/main/Operators.java
@@ -2,4 +2,26 @@ package com.qa.main;
 
 public class Operators {
 
+//	Operators 
+//	Add      +
+//	Sub      -
+//	Divide   /
+//	Multiply *
+//  Modulus  %
+	
+	public static int divideMethod() {
+		int result = 10/3;
+		return result;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- Operators/src/com/qa/main/Runner.java
+++ Operators/src/com/qa/main/Runner.java
@@ -2,4 +2,14 @@ package com.qa.main;
 
 public class Runner {
 
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println(Operators.divideMethod());
+		
+		
+		
+		
+	}
 }

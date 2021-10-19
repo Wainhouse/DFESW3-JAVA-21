f2786a46715836bbc7f721a2c92567c64ad58c12
Luke Wainhouse <lukewainhouse@hotmail.com>
2021-10-19 13:20:12 +0200

    --all

---
+++ DFESW3 D2/src/com/qa/main/Fav2.java
@@ -0,0 +1,22 @@
+package com.qa.main;
+
+public class Fav2 {
+	
+	public static void HelloLuke() {
+		System.out.println("Hello Luke");	
+	}
+	
+	public static void FavPizTop() {
+		System.out.println("Spicy Beef");
+		
+	}
+	
+	public static void FavWord() {
+		System.out.println("Moist");
+	}
+	
+	public static void FavNum() {
+		System.out.println("180");
+		
+	}
+}
---
+++ DFESW3 D2/src/com/qa/main/Main.java
@@ -0,0 +1,11 @@
+package com.qa.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Fav2.FavNum();
+		Fav2.FavPizTop();
+		Fav2.FavWord();
+	}
+}

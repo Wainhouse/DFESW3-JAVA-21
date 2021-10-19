dd6d5787b4980fa760e0e365b0a462db6055a06e
Luke Wainhouse <lukewainhouse@hotmail.com>
2021-10-19 15:44:27 +0200

    Finished Operator.java task

---
+++ Operators/src/com/qa/main/Calculator.java
@@ -0,0 +1,58 @@
+package com.qa.main;
+
+public class Calculator {
+
+//	Operators 
+//	Add      +
+//	Sub      -
+//	Divide   /
+//	Multiply *
+//  Modulus  %
+	
+	public static int divideMethod() {
+		int result = 10/3;
+		return result;
+	}
+	
+	public static int addNums(int num1, int num2) {
+		//int result = 5 + 5;
+		int result = num1 + num2;
+		return result;
+	}
+	
+	public static int minNums(int num1, int num2) {
+		//int result = 5 + 5;
+		int result = num1 - num2;
+		return result;
+	}
+	
+	public static int divNums(int num1, int num2) {
+		//int result = 5 + 5;
+		int result = num1 / num2;
+		return result;
+	}
+	
+	public static int multiNums(int num1, int num2) {
+		//int result = 5 + 5;
+		int result = num1 * num2;
+		return result;
+	}
+	
+	public static int sqNums(int num1) {
+		//int result = 5 + 5;
+		int result = num1 * num1;
+		return result;
+	}
+	
+	public static int cbNums(int num1) {
+		//int result = 5 + 5;
+		int result = num1 * num1 * num1;
+		return result;
+	}
+	
+	
+	
+	
+	
+	
+}
--- Operators/src/com/qa/main/Runner.java
+++ Operators/src/com/qa/main/Runner.java
@@ -6,10 +6,12 @@ public class Runner {
 		
 		
 		
-		System.out.println(Operators.divideMethod());
-		
-		
-		
+		System.out.println(Calculator.addNums(20, 25));
+		System.out.println(Calculator.minNums(10, 7));
+		System.out.println(Calculator.divNums(100, 4));
+		System.out.println(Calculator.multiNums(25, 6));
+		System.out.println(Calculator.sqNums(3));
+		System.out.println(Calculator.cbNums(3));
 		
 	}
 }

827a513fc47a0de6b74d754ca09e6e8918dc6f8b
Luke Wainhouse <lukewainhouse@hotmail.com>
2021-10-19 15:16:29 +0200

    Added Operator and Datatypes

---
+++ Datatypes/src/com/qa/main/Demo.java
@@ -0,0 +1,14 @@
+package com.qa.main;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	
+	System.out.println(returns.boo());
+	System.out.println(returns.rope());
+	System.out.println(returns.dec());
+	System.out.println(returns.doub());
+	System.out.println(returns.x());
+	System.out.println(returns.shorty());
+}
+	}
---
+++ Datatypes/src/com/qa/main/returns.java
@@ -0,0 +1,39 @@
+package com.qa.main;
+//
+//Exercise - Create a class called returns
+//Write methods that return the following:
+//
+//- char
+//- float
+//- boolean
+//- double
+//- String
+//Make your main method print the methods returning data types
+
+public class returns {
+	
+	
+	public static float dec() {
+		return 1.6f; 
+	}
+	public static char x() {
+		return 'x';
+	}
+	public static boolean boo() {
+		return true;
+	}
+	public static double doub() {
+		return 2.1245654324D;
+	}
+	public static short shorty() {
+		return 500;	
+	}
+	public static String rope() {
+		return "Peel";
+	}
+	}
+	
+
+
+
+

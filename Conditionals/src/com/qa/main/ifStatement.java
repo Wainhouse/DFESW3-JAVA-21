60f19f7013150564e23fe9be8f63a44b39fc47fe
Luke Wainhouse <lukewainhouse@hotmail.com>
2021-10-20 10:41:53 +0200

    LW added conditionals

---
+++ Conditionals/src/com/qa/main/ifStatement.java
@@ -0,0 +1,63 @@
+package com.qa.main;
+
+public class ifStatement {
+	
+	
+
+	public static void conditionalDemo() {
+		
+		//Java uses if else statements (like basically every programming / scripting language)
+		// computing / programming is binary true / false 
+		
+	
+	boolean isKettleOn = true;
+	
+//	isKettleOn = false;
+		
+//	if is the command word 
+//	next to if within () is the statement
+//	= - assignment
+//	== comparison (checking the value )
+	
+	if (isKettleOn == true) {
+		
+		// is the body 
+		// what the code does if the if statement is true
+		
+		System.out.println("I'll take milk, no sugar");
+		
+	} else if (isKettleOn == false) {
+		
+		System.out.println("Pop the kettle on!");
+		
+	}
+	
+	boolean binsOut = true;
+	
+	if(binsOut) {
+		
+		System.out.println("Bins are out!");
+		
+	} else { 
+		
+		System.out.println("Bins were forgotten");
+	
+		}
+	}
+	
+	// Conditional Operators 
+	// == - equals
+	// > - greater than 
+	// < - less than 
+	// <= less than OR equal
+	// >= greater than OR equal  
+	// || - OR
+	// && - AND
+	// != - not equal
+	
+	// Task - Using JAva IF ELSE create an if elsee stack that does the following:
+	// - prints out "Greater than" if a number is greater than 10 
+	// - prints out "Equals" if a number is equal to 10
+	// - Prints out "
+	
+}
